cacc0f999451dc9ebb80c8662d8a4f7a7434770f
myusername <Matanfriedleet@hotmail.com>
2020-09-06 22:51:11 +0300

    fixes added to SQL queries

--- routes/userApi.js
+++ routes/userApi.js
@@ -52,7 +52,7 @@ userRouter.get('/:id', async function (req, res) {
     try {
         const userData = await sequelize
             .query(
-                `SELECT id, username, imageURL, userRole, coverImgURL
+                `SELECT id, username, imageURL, userRole
              FROM Users
              WHERE Users.id LIKE '%${id}%'`
             )
